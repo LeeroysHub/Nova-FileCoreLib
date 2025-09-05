48dfb556bfc6d6b278c6a3c63a94b5b87c6a163c
Leeroy <leeroy@nowhere.com>
2025-09-05 20:17:30 +1000

    Full LeeroyFlix rebrand in Source Code

--- jni/utils/JNIPing.c
+++ jni/utils/JNIPing.c
@@ -5,10 +5,10 @@
 int get_local_ip(char *target);
 int get_ttl_ip(char* target, int ttl);
 
-jint Java_com_archos_filecorelibrary_samba_SambaDiscovery_findDoubleNatIp(JNIEnv * jenv) {
+jint Java_org_leeroy_filecorelibrary_samba_SambaDiscovery_findDoubleNatIp(JNIEnv * jenv) {
 	return htonl(get_ttl_ip("8.8.8.8", 2));
 }
 
-jint Java_com_archos_filecorelibrary_samba_SambaDiscovery_findLocalIp(JNIEnv * jenv) {
+jint Java_org_leeroy_filecorelibrary_samba_SambaDiscovery_findLocalIp(JNIEnv * jenv) {
 	return htonl(get_local_ip("8.8.8.8"));
 }
---
+++ src/org/leeroy/filecorelibrary/smbj/SmbjUtils.java
@@ -0,0 +1,206 @@
+// Copyright 2023 Courville Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.leeroy.filecorelibrary.smbj;
+
+import static org.leeroy.filecorelibrary.FileUtils.getShareName;
+
+import android.content.Context;
+import android.net.Uri;
+
+import androidx.preference.PreferenceManager;
+
+import org.leeroy.filecorelibrary.jcifs.JcifsUtils;
+import org.leeroy.filecorelibrary.samba.NetworkCredentialsDatabase;
+import org.leeroy.filecorelibrary.samba.SambaDiscovery;
+
+import com.hierynomus.msfscc.FileAttributes;
+import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
+
+import com.hierynomus.mssmb2.SMBApiException;
+import com.hierynomus.protocol.commons.EnumWithValue;
+import com.hierynomus.security.bc.BCSecurityProvider;
+import com.hierynomus.smbj.SMBClient;
+import com.hierynomus.smbj.SmbConfig;
+import com.hierynomus.smbj.auth.AuthenticationContext;
+import com.hierynomus.smbj.common.SMBRuntimeException;
+import com.hierynomus.smbj.connection.Connection;
+import com.hierynomus.smbj.session.SMB2GuestSigningRequiredException;
+import com.hierynomus.smbj.session.Session;
+import com.hierynomus.smbj.share.DiskShare;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SmbjUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(SmbjUtils.class);
+    static private ConcurrentHashMap<NetworkCredentialsDatabase.Credential, Session> smbjSessions = new ConcurrentHashMap<>();
+    static private ConcurrentHashMap<NetworkCredentialsDatabase.Credential, DiskShare> smbjShares = new ConcurrentHashMap<>();
+    static private ConcurrentHashMap<NetworkCredentialsDatabase.Credential, Connection> smbjConnections = new ConcurrentHashMap<>();
+    private static Context mContext;
+    // singleton, volatile to make double-checked-locking work correctly
+    private static volatile SmbjUtils sInstance;
+
+    private static SmbConfig smbConfig;
+
+    // get the instance, context is used for initial context injection
+    public static SmbjUtils getInstance(Context context) {
+        if (context == null) log.warn("getInstance: context passed is null!!!");
+        else if (mContext == null) mContext = context;
+        if (sInstance == null) {
+            synchronized(SmbjUtils.class) {
+                if (sInstance == null) sInstance = new SmbjUtils(context.getApplicationContext());
+            }
+        }
+        return sInstance;
+    }
+
+    /** may return null but no Context required */
+    public static SmbjUtils peekInstance() {
+        return sInstance;
+    }
+
+    private SmbjUtils(Context context) {
+        mContext = context;
+        log.debug("SmbjUtils: initializing contexts");
+        smbConfig = SmbConfig.builder().withSecurityProvider(new BCSecurityProvider()).withMultiProtocolNegotiate(true).withSigningRequired(false)
+                .build();
+    }
+
+    public synchronized void getSmbConnection(Uri uri) throws IOException, SMBApiException {
+        NetworkCredentialsDatabase.Credential cred = NetworkCredentialsDatabase.getInstance().getCredential(uri.toString());
+        if (cred == null)
+            cred = new NetworkCredentialsDatabase.Credential("anonymous", "", buildKeyFromUri(uri).toString(), "", true);
+        String server = uri.getHost();
+        String password = cred.getPassword();
+        String username = cred.getUsername();
+        String domain = cred.getDomain();
+        int port = uri.getPort();
+        Connection smbConnection = smbjConnections.get(cred);
+        if (smbConnection == null || !smbConnection.isConnected()) {
+            log.trace("getSmbConnection: smbConnection is null or not connected for {}, connecting to {}", uri, server);
+            SMBClient smbClient;
+            if (smbConfig != null) smbClient = new SMBClient(smbConfig);
+            else smbClient = new SMBClient();
+            String serverIP = SambaDiscovery.getIpFromShareName(server);
+            if (serverIP == null)
+                serverIP = JcifsUtils.getInstance(mContext).getBaseContextOnly(true).getNameServiceClient().getByName(server).getHostAddress();
+            log.trace("getSmbConnection: {} -> {}", server, serverIP);
+            smbConnection = (port != -1) ? smbClient.connect(serverIP, port) : smbClient.connect(serverIP);
+            smbjConnections.put(cred, smbConnection);
+            // check that auth information is valid
+            if (username == null || password == null) {
+                log.error("getSmbConnection: username or password is null for uri {}", uri);
+                throw new IOException("Invalid credentials: username or password is null");
+            }
+            // need to regenerate smbSession in this case too
+            AuthenticationContext ac = new AuthenticationContext(username, password.toCharArray(), domain);
+            if (ac == null) {
+                log.error("getSmbConnection: AuthenticationContext is null for uri {}", uri);
+                throw new IOException("Failed to create AuthenticationContext");
+            }
+            Session smbSession;
+            try {
+                smbSession = smbConnection.authenticate(ac);
+            } catch (SMB2GuestSigningRequiredException e) {
+                log.error("getSmbConnection: caught SMB2GuestSigningRequiredException {} for uri {} -> throwing IOException instead", e.getMessage(), uri);
+                throw new IOException("getSmbConnection: SMB2GuestSigningRequiredException");
+            }
+            smbjSessions.put(cred, smbSession);
+        }
+    }
+
+    public synchronized DiskShare getSmbShare(Uri uri) throws IOException, SMBApiException {
+        NetworkCredentialsDatabase.Credential cred = NetworkCredentialsDatabase.getInstance().getCredential(uri.toString());
+        if (cred == null)
+            cred = new NetworkCredentialsDatabase.Credential("anonymous", "", buildKeyFromUri(uri).toString(), "", true);
+        String shareName = getShareName(uri);
+        getSmbConnection(uri); // be sure to be connected
+        // shareName can be null when asking for smbj://server/
+        if (shareName == null) {
+            log.warn("getSmbShare: returning null shareName for uri {}", uri);
+            return null;
+        }
+        DiskShare smbShare = smbjShares.get(cred);
+        if (smbShare == null || !shareName.equals(getShareName(Uri.parse(cred.getUriString()))) || !smbShare.isConnected()) {
+            log.trace("getSmbShare: smbShare is null or not connected for {}", shareName);
+            // ensures that there is a valid connection and regenerate session if not connected
+            getSmbConnection(uri);
+            Session smbSession = smbjSessions.get(cred);
+            if (smbSession != null) {
+                try {
+                    smbShare = (DiskShare) smbSession.connectShare(shareName);
+                    log.trace("getSmbShare: saving smbShare {}, smbshare={}", shareName, smbShare);
+                    smbjShares.put(cred, smbShare);
+                } catch (SMBRuntimeException e) {
+                    if (e.getCause() instanceof SocketException) {
+                        // Try to reconnect
+                        getSmbConnection(uri);
+                        smbSession = smbjSessions.get(cred);
+                        if (smbSession != null) {
+                            smbShare = (DiskShare) smbSession.connectShare(shareName);
+                            smbjShares.put(cred, smbShare);
+                        } else {
+                            log.error("getSmbShare: caught SMBRuntimeException but smbSession is null after reconnecting! {} for uri {}, sharename={} -> throwing IOException instead", e.getMessage(), uri, shareName);
+                            throw new IOException("getSmbShare: smbSession is null after reconnecting!");
+                        }
+                    } else {
+                        log.error("getSmbShare: caught exception {} for uri {}, sharename={} -> throwing IOException instead", e.getMessage(), uri, shareName);
+                        throw new IOException("getSmbShare: smbSession is null after reconnecting!");
+                    }
+                }
+            } else log.warn("getSmbShare: smbSession is null!");
+        }
+        log.debug("getSmbShare: for uri {}, sharename={}, smbShare={}, isConnected={}", uri, shareName, smbShare, (smbShare != null)?smbShare.isConnected():"false");
+        return smbShare;
+    }
+
+    private static Uri buildKeyFromUri(Uri uri) {
+        // use Uri without the path segment as key: for example, "smbj://blabla.com:5006/toto/titi" gives a "smbj://blabla.com:5006" key
+        return uri.buildUpon().path("").build();
+    }
+
+    public static boolean isDirectory(FileIdBothDirectoryInformation fileEntry) {
+        return EnumWithValue.EnumUtils.isSet(fileEntry.getFileAttributes(), FileAttributes.FILE_ATTRIBUTE_DIRECTORY);
+    }
+
+    public static boolean isSMBjEnabled() {
+        if (mContext == null) {
+            log.error("isSMBjEnabled: mContext is null! Returning false");
+            return false;
+        }
+        log.trace("isSMBjEnabled={}", PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean("pref_smbj", false));
+        return PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean("pref_smbj", false);
+    }
+
+    /**
+     * Invalidate cached DiskShare for a URI, forcing reconnection on next access.
+     * Used when a protocol error (TransportException) indicates connection is compromised.
+     */
+    public synchronized void invalidateShare(Uri uri) {
+        NetworkCredentialsDatabase.Credential cred = NetworkCredentialsDatabase.getInstance().getCredential(uri.toString());
+        if (cred != null) {
+            DiskShare share = smbjShares.remove(cred);
+            if (share != null) {
+                log.debug("invalidateShare: removed cached share for {}", uri);
+            }
+        }
+    }
+
+}
