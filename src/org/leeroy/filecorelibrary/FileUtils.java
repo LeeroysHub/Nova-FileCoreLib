91b3eb096d24f67e0751730d3af3639d337c3b29
Leeroy <leeroy@nowhere.com>
2025-09-08 09:58:34 +1000

    Remove logs

--- src/org/leeroy/filecorelibrary/FileUtils.java
+++ src/org/leeroy/filecorelibrary/FileUtils.java
@@ -348,10 +348,10 @@ public class FileUtils {
         if (uri == null) return null;
         List<String> pathSegments = uri.getPathSegments();
         if (pathSegments != null && !pathSegments.isEmpty()) {
-            log.debug("getShareName: uri=" + uri + " -> " + Arrays.toString(pathSegments.toArray()) + " -> " +  pathSegments.get(0));
+            //log.debug("getShareName: uri=" + uri + " -> " + Arrays.toString(pathSegments.toArray()) + " -> " +  pathSegments.get(0));
             return pathSegments.get(0);
         } else {
-            log.warn("getShareName: returns null since getPathSegments is null for uri=" + uri);
+            //log.warn("getShareName: returns null since getPathSegments is null for uri=" + uri);
         }
         return null;
     }
